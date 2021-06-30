c9edf24243adbbf5b2986dda943989bf9db1b2ba
Mizraim Eliab <mizraimeliab168@gmail.com>
2021-06-29 21:26:49 -0500

    configuration

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'admin',
-    database: 'gallery_db',
-    port: '5432'
+    host: '', //localhost
+    user: '', //postgres
+    password: '', //admin
+    database: '', //gallery_db
+    port: '' //5432
 });
 console.log('Base de datos conectada');
 
