8a7f25663c9f6462d8869a1e4227342ea4a2b99e
Adrian <adrian.avendano@kiubix.com>
2021-06-30 18:54:56 -0500

    Adding character

--- routes/channels.routes.js
+++ routes/channels.routes.js
@@ -6,6 +6,6 @@ router.get('/',channelController.getChannels);
 router.get('/:id',channelController.getChannel);
 router.post('/',channelController.postChannel);
 router.put('/:id',channelController.putChannel);
-router.put('/delete/:id',channelController.deleteChannel)
+router.put('/delete/:id',channelController.deleteChannel);
 
 module.exports = router;
