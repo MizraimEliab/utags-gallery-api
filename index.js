bed2a9e8081033a01e6d0d0f4a4cf4a39f8161e3
Mizraim Eliab <mizraimeliab168@gmail.com>
2021-07-21 19:44:24 -0500

    add different configuration

--- index.js
+++ index.js
@@ -4,23 +4,12 @@ const app = express();
 const {pool} = require('./database');
 const cors = require('cors');
 
-const whitelist = ['https://utagsgallery-codes.herokuapp.com', 'https://api-utagsgallery-codes.herokuapp.com','localhost:4200']
-
-const corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    }
-}
 
 app.set('port',process.env.PORT || 3000);
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors(corsOptions)); //http://localhost:4200 {origin: 'https://utagsgallery-codes.herokuapp.com'}
+app.use(cors({origin: ['https://utagsgallery-codes.herokuapp.com', 'localhost:4200']})); //http://localhost:4200 {origin: 'https://utagsgallery-codes.herokuapp.com'}
 
 app.use('/users',require('./routes/users.routes'));
 app.use('/channels',require('./routes/channels.routes'));
